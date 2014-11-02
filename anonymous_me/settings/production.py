19af69ce91ba3cc491e11de4210d20bfcc94962a
black-perl <ankush.sharma.ece12@itbhu.ac.in>
2014-11-03 02:31:17 +0530

    Secret Push

--- anonymous_me/settings/production.py
+++ anonymous_me/settings/production.py
@@ -2,6 +2,7 @@ from base import *
 import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+DATABASE_URL = 
 
 DEBUG = False
 TEMPLATE_DEBUG = False
@@ -12,8 +13,18 @@ WSGI_APPLICATION = 'anonymous_me.wsgi_production.application'
 # Database settings
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbpf1qhlrdvrej',
+        'HOST': 'ec2-54-83-204-85.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'jpyobasmfwtoys',
+        'PASSWORD': '4e_vRRrZ80sLm-1h048SrdzLyM',
+    }
+}
+
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

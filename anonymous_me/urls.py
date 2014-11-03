078fb91f351f694900db1ce0715285dd7a01383f
black-perl <ankush.sharma.ece12@itbhu.ac.in>
2014-11-03 10:24:29 +0530

    Corrected url regex matching

--- anonymous_me/urls.py
+++ anonymous_me/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
+
+from django.http import HttpResponse
 from url_shortener import views
 from django.views.generic import TemplateView 
 
@@ -8,7 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^$',views.home),
     url(r'^shortify_url$',views.shortify_url),
-    url(r'^imankush/', include(admin.site.urls)),
+    url(r'^imankush$', include(admin.site.urls)),
     url(r'^google4962e1a518060429\.html$', lambda r: HttpResponse("google-site-verification: google4962e1a518060429.html", mimetype="text/plain")), #added google site ownership verification file
 	url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')), #added ronots.txt file
 	url(r'^sitemap.xml$',TemplateView.as_view(template_name='sitemap.xml')), #serving sitemap
